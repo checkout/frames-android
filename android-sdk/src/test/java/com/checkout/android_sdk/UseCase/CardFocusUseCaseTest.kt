8ed7f6a4110402234c0190a0b1dcc66ad79f96ab
John O'Halloran <john.ohalloran@checkout.com>
2019-02-27 17:01:56 +0000

    Change to bdd style testing.

--- android-sdk/src/test/java/com/checkout/android_sdk/UseCase/CardFocusUseCaseTest.kt
+++ android-sdk/src/test/java/com/checkout/android_sdk/UseCase/CardFocusUseCaseTest.kt
@@ -2,8 +2,8 @@ package com.checkout.android_sdk.UseCase
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.BDDMockito.then
 import org.mockito.Mock
-import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
@@ -16,21 +16,24 @@ class CardFocusUseCaseTest {
     fun `given valid card without focus then card focus result is error`() {
         CardFocusUseCase(false, "1234567812345678", callbackMock).execute()
 
-        verify(callbackMock).onCardFocusResult(true)
+        then(callbackMock).should()
+            .onCardFocusResult(true)
     }
 
     @Test
     fun `given valid card with focus then card focus result is no error`() {
         CardFocusUseCase(true, "4242424242424242", callbackMock).execute()
 
-        verify(callbackMock).onCardFocusResult(false)
+        then(callbackMock).should()
+            .onCardFocusResult(false)
     }
 
     @Test
     fun `given invalid card without focus then card focus result is error`() {
         CardFocusUseCase(false, "11", callbackMock).execute()
 
-        verify(callbackMock).onCardFocusResult(true)
+        then(callbackMock).should()
+            .onCardFocusResult(true)
     }
 
 }
