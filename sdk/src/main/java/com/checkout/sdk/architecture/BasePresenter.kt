bf3b9f42d01df5553b0aa046575589f9baa3932d
John O'Halloran <john.ohalloran@checkout.com>
2019-03-26 13:21:45 +0000

    Restrict `set` access of uiState in the BasePresenter.

--- sdk/src/main/java/com/checkout/sdk/architecture/BasePresenter.kt
+++ sdk/src/main/java/com/checkout/sdk/architecture/BasePresenter.kt
@@ -6,9 +6,12 @@ package com.checkout.sdk.architecture
  * - keeping track of ui state
  * - safely updating the view
  */
-abstract class BasePresenter<in V : MvpView<U>, U: UiState> (protected var uiState: U) {
+abstract class BasePresenter<in V : MvpView<U>, U: UiState> (uiState: U) {
 
     private var view: V? = null
+
+    protected var uiState: U = uiState
+        private set
 
     fun start(view: V) {
         this.view = view
