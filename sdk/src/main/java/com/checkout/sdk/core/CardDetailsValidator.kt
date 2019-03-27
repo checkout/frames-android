89f42d871a697c291b71cdcaf05132674197d180
John O'Halloran <john.ohalloran@checkout.com>
2019-03-27 16:50:03 +0000

    Add test for PayButtonClickedUseCase.

--- sdk/src/main/java/com/checkout/sdk/core/CardDetailsValidator.kt
+++ sdk/src/main/java/com/checkout/sdk/core/CardDetailsValidator.kt
@@ -3,9 +3,9 @@ package com.checkout.sdk.core
 import com.checkout.sdk.store.InMemoryStore
 
 
-class CardDetailsValidator(private val store: InMemoryStore) {
+open class CardDetailsValidator(private val store: InMemoryStore) {
 
-    fun getValidity(): CardDetailsValidity {
+    open fun getValidity(): CardDetailsValidity {
         return CardDetailsValidity(
             store.cardNumber.isValid(),
             store.cvv.isValid(),
