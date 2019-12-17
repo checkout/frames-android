69a9d10b034bd3e68e63e9d1d615dea6dd09267c
ioan-ghisoi-cko <ioan.ghisoi@checkout.com>
2019-12-17 11:33:18 +0000

    Ignore default Locale in DateFormatter - avoid API validation issue

--- android-sdk/src/main/java/com/checkout/android_sdk/Utils/DateFormatter.kt
+++ android-sdk/src/main/java/com/checkout/android_sdk/Utils/DateFormatter.kt
@@ -10,8 +10,8 @@ open class DateFormatter {
      * Turn the month integer into a formatted String: 1 -> 01 etc
      */
     open fun formatMonth(monthInteger: Int): String {
-        val monthParse = SimpleDateFormat("MM", Locale.getDefault())
-        val monthDisplay = SimpleDateFormat("MM", Locale.getDefault())
+        val monthParse = SimpleDateFormat("MM", Locale.UK)
+        val monthDisplay = SimpleDateFormat("MM", Locale.UK)
         return monthDisplay.format(monthParse.parse(monthInteger.toString()))
     }
 
