2d2156113aa95b6ec73e7753adc7fda68256b6e6
Chintan <chintan.soni@checkout.com>
2022-08-22 20:41:20 +0100

    PIMOB-1393: Expirydate smart logic was implemented

--- frames/src/main/java/com/checkout/frames/component/expirydate/ExpiryDateViewModel.kt
+++ frames/src/main/java/com/checkout/frames/component/expirydate/ExpiryDateViewModel.kt
@@ -1,18 +1,23 @@
 package com.checkout.frames.component.expirydate
 
-import android.util.Log
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.checkout.base.mapper.Mapper
+import com.checkout.frames.base.validation.ValidateResult
 import com.checkout.frames.component.base.InputComponentState
+import com.checkout.frames.component.expirydate.usecase.ValidateExpiryDateUseCase
 import com.checkout.frames.di.base.InjectionClient
 import com.checkout.frames.di.base.Injector
 import com.checkout.frames.di.component.ExpiryDateViewModelSubComponent
 import com.checkout.frames.style.component.ExpiryDateComponentStyle
 import com.checkout.frames.style.component.base.InputComponentStyle
 import com.checkout.frames.style.view.InputComponentViewStyle
+import com.checkout.frames.utils.constants.InputFieldConstants.EXPIRY_DATE_MAXIMUM_LENGTH
+import com.checkout.frames.view.InputFieldState
 import com.checkout.tokenization.model.ExpiryDate
 import com.checkout.validation.api.CardValidator
 import com.checkout.validation.model.ValidationResult
@@ -27,22 +32,68 @@ internal class ExpiryDateViewModel @Inject constructor(
     private val style: ExpiryDateComponentStyle,
 ) : ViewModel() {
 
-    val componentState = ExpiryDateComponentState(inputComponentStateMapper.map(style.inputStyle))
+    val componentState = provideViewState(style.inputStyle)
     val componentStyle = provideViewStyle(style.inputStyle)
 
-    fun onExpiryDateInputChange(text: String) {
-        componentState.inputState.inputFieldState.text.value = text
-        handleValidationResult(cardValidator.validateExpiryDate(text, text))
+    // Flag to determine that component has been already focused before
+    // Needed to prevent validation on focus switch for initial component state
+    private var wasFocused = false
+
+    fun onFocusChanged(isFocused: Boolean) {
+        if (isFocused) wasFocused = isFocused
+
+        if (!isFocused && wasFocused) {
+            val expiryDate = componentState.expiryDate.value
+            validateExpiryDateOnFocusChanged(expiryDate)
+        }
     }
 
-    // TODO: PIMOB-1393 - Implement smart logic for expiry date.
-    private fun handleValidationResult(validateExpiryDate: ValidationResult<ExpiryDate>) {
-     Log.d("System out", "handleValidationResult $validateExpiryDate")
+    private fun validateExpiryDateOnFocusChanged(expiryDate: String) {
+        if (expiryDate.length >= EXPIRY_DATE_MAXIMUM_LENGTH) {
+            handleValidationResult(cardValidator.validateExpiryDate(expiryDate.take(2), expiryDate.takeLast(2)))
+        } else {
+            // TODO: PIMOB-1401 - Implement error handling for expiry date.
+        }
+    }
+
+    fun onExpiryDateInputChange(text: String) {
+        val strBuilder = StringBuilder()
+        strBuilder.append(text)
+
+        when (val validateExpiryDateUseCase = ValidateExpiryDateUseCase().execute(strBuilder)) {
+            is ValidateResult.Success -> {
+                componentState.inputState.inputFieldState.text.value = validateExpiryDateUseCase.value
+            }
+            is ValidateResult.Failure -> {
+                componentState.inputState.inputFieldState.text.value = validateExpiryDateUseCase.data
+                // TODO: PIMOB-1401 - Implement error handling for expiry date.
+            }
+        }
+    }
+
+    private fun handleValidationResult(result: ValidationResult<ExpiryDate>) = when (result) {
+        is ValidationResult.Success -> with(result.value) { }
+        is ValidationResult.Failure -> {
+            // TODO: PIMOB-1401 - Implement error handling for expiry date.
+        }
+    }
+
+    private fun provideViewState(inputStyle: InputComponentStyle): ExpiryDateComponentState {
+        var viewState = ExpiryDateComponentState(inputComponentStateMapper.map(inputStyle))
+        val inputFieldState = InputFieldState(maxLength = mutableStateOf(EXPIRY_DATE_MAXIMUM_LENGTH))
+
+        viewState = viewState.copy(
+            inputState = viewState.inputState.copy(
+                inputFieldState = inputFieldState
+            )
+        )
+
+        return viewState
     }
 
     private fun provideViewStyle(inputStyle: InputComponentStyle): InputComponentViewStyle {
         var viewStyle = inputComponentStyleMapper.map(inputStyle)
-        val keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        val keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next)
 
         viewStyle = viewStyle.copy(
             inputFieldStyle = viewStyle.inputFieldStyle.copy(
