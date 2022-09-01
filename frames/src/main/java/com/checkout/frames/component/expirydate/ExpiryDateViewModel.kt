7e07669135048d41db18a5311d85a5c505810aa7
Chintan <chintan.soni@checkout.com>
2022-09-01 16:18:25 +0100

    PIMOB-1401: Error messages refactored after merge.

--- frames/src/main/java/com/checkout/frames/component/expirydate/ExpiryDateViewModel.kt
+++ frames/src/main/java/com/checkout/frames/component/expirydate/ExpiryDateViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.checkout.base.mapper.Mapper
 import com.checkout.base.usecase.UseCase
+import com.checkout.frames.R
 import com.checkout.frames.component.base.InputComponentState
 import com.checkout.frames.component.expirydate.model.SmartExpiryDateValidationRequest
 import com.checkout.frames.di.base.InjectionClient
@@ -19,6 +20,7 @@ import com.checkout.frames.utils.constants.EXPIRY_DATE_DIGITS_PATTERN
 import com.checkout.frames.utils.constants.EXPIRY_DATE_MAXIMUM_LENGTH_FOUR
 import com.checkout.frames.utils.constants.EXPIRY_DATE_MAXIMUM_LENGTH_THREE
 import com.checkout.frames.utils.constants.EXPIRY_DATE_ZERO_POSITION_CHECK
+import com.checkout.validation.error.ValidationError.Companion.EXPIRY_DATE_IN_PAST
 import com.checkout.validation.model.ValidationResult
 import javax.inject.Inject
 import javax.inject.Provider
@@ -54,27 +56,30 @@ internal class ExpiryDateViewModel @Inject constructor(
     }
 
     private fun updateExpiryDateMaxLength(smartLogicExpiryDateUseCase: ValidationResult.Success<String>) {
-        if (
+        componentState.expiryDateMaxLength.value = if (
             smartLogicExpiryDateUseCase.value.isNotEmpty() &&
             smartLogicExpiryDateUseCase.value.first() > EXPIRY_DATE_ZERO_POSITION_CHECK
-        ) componentState.expiryDateMaxLength.value = EXPIRY_DATE_MAXIMUM_LENGTH_THREE
-        else componentState.expiryDateMaxLength.value = EXPIRY_DATE_MAXIMUM_LENGTH_FOUR
+        ) EXPIRY_DATE_MAXIMUM_LENGTH_THREE else EXPIRY_DATE_MAXIMUM_LENGTH_FOUR
     }
 
     private fun validateExpiryDate(expiryDate: String, isFocused: Boolean) {
         val smartExpiryDateValidationRequest = SmartExpiryDateValidationRequest(isFocused, expiryDate)
 
-        handleValidationResult(smartExpiryDateValidationUseCase.execute(smartExpiryDateValidationRequest))
+        handleValidationResult(smartExpiryDateValidationUseCase.execute(smartExpiryDateValidationRequest), isFocused)
     }
 
-    private fun handleValidationResult(result: ValidationResult<String>) = when (result) {
+    private fun handleValidationResult(result: ValidationResult<String>, isFocused: Boolean) = when (result) {
         is ValidationResult.Success -> {
             updateExpiryDateMaxLength(result)
             componentState.inputState.inputFieldState.text.value = result.value
+            componentState.hideError()
         }
 
         is ValidationResult.Failure -> {
-            result.error.message?.let { componentState.showError(it) }
+            if (isFocused && result.error.errorCode == EXPIRY_DATE_IN_PAST)
+                componentState.showError(R.string.cko_base_invalid_past_expiry_date_error)
+            else
+                componentState.showError(R.string.cko_base_invalid_expiry_date_error)
         }
     }
 
