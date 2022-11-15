a24e0b581683ee885d18024d26c45cad92a417ad
Max Nosov <maxim.nosov@checkout.com>
2022-11-15 08:41:54 +0000

    PIMOB-1504: Checkout API is public

--- checkout/src/main/java/com/checkout/CheckoutApiServiceFactory.kt
+++ checkout/src/main/java/com/checkout/CheckoutApiServiceFactory.kt
@@ -1,7 +1,6 @@
 package com.checkout
 
 import android.content.Context
-import androidx.annotation.RestrictTo
 import com.checkout.api.CheckoutApiClient
 import com.checkout.api.CheckoutApiService
 import com.checkout.base.model.Environment
@@ -27,7 +26,6 @@ import com.checkout.validation.validator.PhoneValidator
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
-@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 public object CheckoutApiServiceFactory {
 
     public fun create(
