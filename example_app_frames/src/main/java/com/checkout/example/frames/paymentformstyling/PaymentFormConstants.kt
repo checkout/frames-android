7e4cf5472cbf33c4034fa74815f5e40e26e90e08
Chintan Soni <cvsoni22@gmail.com>
2022-12-06 22:59:39 +0000

    PIMOB-1347: Custom version were added in sample app

---
+++ example_app_frames/src/main/java/com/checkout/example/frames/paymentformstyling/PaymentFormConstants.kt
@@ -0,0 +1,23 @@
+package com.checkout.example.frames.paymentformstyling
+
+import com.checkout.frames.model.CornerRadius
+import com.checkout.frames.model.Shape
+
+object PaymentFormConstants {
+    const val backgroundColor: Long = 0XFFFFD1D9
+    const val inputFieldColor: Long = 0XFFFFFFFF
+    const val textColor: Long = 0XFF461E67
+    const val placeHolderTextColor: Long = 0XFFACA2B4
+    /** Screen padding in dp. */
+    const val padding: Int = 16
+    const val paddingOneDp: Int = 1
+    const val backIconSize: Int = 28
+
+    /** Field bottom margin in dp. */
+    const val marginBottom: Int = 24
+    const val marginTop: Int = 16
+    const val margin: Int = 10
+
+    val inputFieldBorderShape = Shape.CutCorner
+    val inputFieldCornerRadius = CornerRadius(12)
+}
