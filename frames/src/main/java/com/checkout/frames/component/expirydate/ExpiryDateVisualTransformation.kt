8119d93add4199781d6191a6ceca9d17f5c8cd41
Chintan Soni <chintan.soni@checkout.com>
2023-07-11 18:02:49 +0100

    PIMOB-2040: Fixed expiry date crash and update visual transformation logic

--- frames/src/main/java/com/checkout/frames/component/expirydate/ExpiryDateVisualTransformation.kt
+++ frames/src/main/java/com/checkout/frames/component/expirydate/ExpiryDateVisualTransformation.kt
@@ -15,46 +15,64 @@ internal class ExpiryDateVisualTransformation : VisualTransformation {
 
     private fun performExpiryDateFilter(text: AnnotatedString): TransformedText {
         // format: XX/XX
+        /*
+         * Depending on the first number is where the separator will be placed
+         * If the first number is 2-9 then the slash will come after the
+         * 2, if the first number is 11 or 12 it will be after the second digit,
+         * if the number is 01 it will be after the second digit.
+         */
+        val isSingleDigitMonth = text.isNotBlank() && !(text[0] == '0' || text[0] == '1')
 
-        val stringBuilder = StringBuilder()
-        for (i in text.indices) {
-            when {
-                i == 0 && text[i] > EXPIRY_DATE_ZERO_POSITION_CHECK -> {
-                    stringBuilder.append(EXPIRY_DATE_PREFIX_ZERO + text[i] + separator)
+        val output = buildString {
+            for ((index, char) in text.withIndex()) {
+                if (isSingleDigitMonth && index == 0 && char > EXPIRY_DATE_ZERO_POSITION_CHECK) {
+                    append(EXPIRY_DATE_PREFIX_ZERO + char + separator)
+                } else {
+                    append(char)
                 }
-                i == 1 && !stringBuilder.contains(separator) -> {
-                    stringBuilder.append(text[i] + separator)
-                }
-                else -> {
-                    stringBuilder.append(text[i])
+
+                if (index == 1 && !this.contains(separator)) {
+                    append(separator)
                 }
             }
         }
 
-        val expiryDateOffsetTranslator = object : OffsetMapping {
-            override fun originalToTransformed(offset: Int): Int {
-                val withZero = text.isNotEmpty() && text[0] > EXPIRY_DATE_ZERO_POSITION_CHECK
-                val zeroPrefix = if (withZero) 1 else 0
+        val outputOffsets = calculateOutputOffsets(output)
+        val separatorIndices = calculateSeparatorOffsets(output)
 
-                return when {
-                    offset == 0 -> offset
-                    !withZero && offset < 2 -> offset
-                    else -> offset + separator.length + zeroPrefix
-                }
+        val offsetTranslator = object : OffsetMapping {
+
+            override fun originalToTransformed(offset: Int): Int {
+                val updatedOffset: Int = if (isSingleDigitMonth) offset + 1 else offset
+
+                return outputOffsets[updatedOffset]
             }
 
             override fun transformedToOriginal(offset: Int): Int {
-                val withZero = text.isNotEmpty() && text[0] > EXPIRY_DATE_ZERO_POSITION_CHECK
-                val zeroPrefix = if (withZero) 1 else 0
+                val updatedOffset: Int = if (isSingleDigitMonth && offset > 0) offset - 1 else offset
+                val separatorCharactersBeforeOffset = separatorIndices.count { it < offset }
 
-                return when {
-                    offset == 0 -> offset
-                    !withZero && offset < 2 -> offset
-                    else -> offset - separator.length - zeroPrefix
-                }
+                return updatedOffset - separatorCharactersBeforeOffset
             }
         }
 
-        return TransformedText(AnnotatedString(stringBuilder.toString()), expiryDateOffsetTranslator)
+        return TransformedText(AnnotatedString(output), offsetTranslator)
+    }
+
+    private fun calculateOutputOffsets(output: String): List<Int> {
+        val digitOffsets = output.mapIndexedNotNull { index, char ->
+            // +1 because we're looking for offsets, not indices
+            index.takeIf { char.isDigit() }?.plus(1)
+        }
+        // We're adding 0 so that the cursor can be placed at the start of the text,
+        // and replace the last digit offset with the length of the output. The latter
+        // is so that the offsets are set correctly for text such as "04 / ".
+        return listOf(0) + digitOffsets.dropLast(1) + output.length
+    }
+
+    private fun calculateSeparatorOffsets(output: String): List<Int> {
+        return output.mapIndexedNotNull { index, c ->
+            index.takeUnless { c.isDigit() }
+        }
     }
 }
