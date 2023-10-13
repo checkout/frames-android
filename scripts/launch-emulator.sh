ffbbef5d27d41299a28ddcda78d842414fb9f825
Jheng-Hao Lin <Jheng-Hao.Lin@checkout.com>
2023-10-13 23:57:01 +0100

    Experiment using macOs-latest to run

--- scripts/launch-emulator.sh
+++ scripts/launch-emulator.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
-# Please run this AFTER running scripts/setup-android-env.sh
+# Install required packages
 yes | $ANDROID_SDK_ROOT/cmdline-tools/latest/bin/sdkmanager --package_file=./scripts/android-sdk-packages  --sdk_root=$ANDROID_SDK_ROOT
 
 # Create an AVD
 $ANDROID_SDK_ROOT/cmdline-tools/latest/bin/avdmanager create avd -n testingAVD -d pixel --package "system-images;android-29;google_apis;x86_64"
 
-# List AVDs
-$ANDROID_SDK_ROOT/emulator/emulator -list-avds
-
-# Update
-$ANDROID_SDK_ROOT/cmdline-tools/latest/bin/sdkmanager --update
-
 # Start an emulator
 $ANDROID_SDK_ROOT/emulator/emulator -avd testingAVD -no-accel -no-window -no-audio -no-snapshot -no-boot-anim -camera-back none -camera-front none &
-#$ANDROID_SDK_ROOT/emulator/emulator -avd testingAVD -no-audio -no-window -no-boot-anim -no-snapshot -camera-back none -camera-front none -selinux permissive -qemu -vnc :0
 
