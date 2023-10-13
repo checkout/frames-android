66a540efb6ff00e46c470e2af47f02ea1e9da15e
Jheng-Hao Lin <Jheng-Hao.Lin@checkout.com>
2023-10-13 23:49:46 +0100

    Experiment using macOs-latest to run

--- scripts/install-android-sdk-packages.sh
+++ scripts/install-android-sdk-packages.sh
@@ -9,7 +9,10 @@ $ANDROID_SDK_ROOT/cmdline-tools/latest/bin/avdmanager create avd -n testingAVD -
 # List AVDs
 $ANDROID_SDK_ROOT/emulator/emulator -list-avds
 
+# Update
+$ANDROID_SDK_ROOT/cmdline-tools/latest/bin/sdkmanager --update
+
 # Start an emulator
-$ANDROID_SDK_ROOT/emulator/emulator -avd testingAVD -no-accel -no-window -no-audio -no-snapshot -no-boot-anim -camera-back none -camera-front none
+$ANDROID_SDK_ROOT/emulator/emulator -avd testingAVD -no-accel -no-window -no-audio -no-snapshot -no-boot-anim -camera-back none -camera-front none &
 #$ANDROID_SDK_ROOT/emulator/emulator -avd testingAVD -no-audio -no-window -no-boot-anim -no-snapshot -camera-back none -camera-front none -selinux permissive -qemu -vnc :0
 
