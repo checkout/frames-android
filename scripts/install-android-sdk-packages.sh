41de53979ab6124754b1652eeaffd87a385a615d
Jheng-Hao Lin <Jheng-Hao.Lin@checkout.com>
2023-10-13 23:43:47 +0100

    Experiment using macOs-latest to run

--- scripts/install-android-sdk-packages.sh
+++ scripts/install-android-sdk-packages.sh
@@ -6,7 +6,10 @@ yes | $ANDROID_SDK_ROOT/cmdline-tools/latest/bin/sdkmanager --package_file=./scr
 # Create an AVD
 $ANDROID_SDK_ROOT/cmdline-tools/latest/bin/avdmanager create avd -n testingAVD -d pixel --package "system-images;android-29;google_apis;x86_64"
 
+# List AVDs
+$ANDROID_SDK_ROOT/emulator/emulator -list-avds
+
 # Start an emulator
-$ANDROID_SDK_ROOT/emulator/emulator -avd testingAVD -no-window -no-audio -no-snapshot -no-boot-anim -camera-back none -camera-front none
+$ANDROID_SDK_ROOT/emulator/emulator -avd testingAVD -no-accel -no-window -no-audio -no-snapshot -no-boot-anim -camera-back none -camera-front none
 #$ANDROID_SDK_ROOT/emulator/emulator -avd testingAVD -no-audio -no-window -no-boot-anim -no-snapshot -camera-back none -camera-front none -selinux permissive -qemu -vnc :0
 
