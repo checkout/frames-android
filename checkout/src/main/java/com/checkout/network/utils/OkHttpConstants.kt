bcccf897d1460cd2c4c12c889526d6985fb00c8e
fabio-insolia-cko <123394131+fabio-insolia-cko@users.noreply.github.com>
2024-02-23 10:09:40 +0000

    feature/PIMOB-2024_UserAgent_with_incorrect_format (#265)

--- checkout/src/main/java/com/checkout/network/utils/OkHttpConstants.kt
+++ checkout/src/main/java/com/checkout/network/utils/OkHttpConstants.kt
@@ -1,5 +1,7 @@
 package com.checkout.network.utils
 
+import com.checkout.BuildConfig
+
 internal object OkHttpConstants {
     const val CALL_TIMEOUT_MS = 10000L
     const val CONNECTION_TIMEOUT_MS = 10000L
@@ -7,5 +9,5 @@ internal object OkHttpConstants {
 
     const val HEADER_AUTHORIZATION = "Authorization"
     const val HEADER_USER_AGENT_NAME = "User-Agent"
-    const val HEADER_USER_AGENT_VALUE = "checkout-android-sdk"
+    const val HEADER_USER_AGENT_VALUE = "checkout-sdk-android/${BuildConfig.PRODUCT_VERSION}"
 }
