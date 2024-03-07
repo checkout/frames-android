c3b3ae6a5a924ac892d73376d6bafecc6991cc65
Chintan Soni <114917119+chintan-soni-cko@users.noreply.github.com>
2024-03-07 02:17:32 -0800

    RELEASE:27651 - Release SDK 4.2.1 (#267)

--- buildSrc/src/main/java/FramesConfig.kt
+++ buildSrc/src/main/java/FramesConfig.kt
@@ -1,5 +1,5 @@
 object FramesConfig {
-    const val productVersion = "4.2.0"
+    const val productVersion = "4.2.1"
     const val productArtefactId = "frames-android"
     const val productGroupId = "com.github.checkout"
     const val loggingGroupId = "com.checkout"
