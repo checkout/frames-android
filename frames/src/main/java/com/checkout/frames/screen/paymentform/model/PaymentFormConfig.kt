80963da36cc8d00d24dfb61f8263e321d149bb22
Zane Smith <zane.manzoorsmith@checkout.com>
2026-03-03 16:09:11 +0000

    PIMOB-4287: update kdoc

--- frames/src/main/java/com/checkout/frames/screen/paymentform/model/PaymentFormConfig.kt
+++ frames/src/main/java/com/checkout/frames/screen/paymentform/model/PaymentFormConfig.kt
@@ -14,7 +14,7 @@ import com.checkout.frames.style.screen.PaymentFormStyle
  * @param paymentFlowHandler - [PaymentFlowHandler] represent the handler for PaymentForm
  * @param supportedCardSchemeList - represent the supported card schemes [CardScheme] in PaymentForm
  * @param prefillData - [PrefillData] represent the data for prefill in the PaymentForm
- * @param baseURLPrefix - a prefix for the base url domain, prefix must be alphanumeric
+ * @param baseURLPrefix - an optional alphanumeric prefix used to route requests to a specific regional or merchant-specific subdomain (e.g., "msdd"). Must be alphanumeric.
  */
 public data class PaymentFormConfig @JvmOverloads constructor(
     public val publicKey: String,
